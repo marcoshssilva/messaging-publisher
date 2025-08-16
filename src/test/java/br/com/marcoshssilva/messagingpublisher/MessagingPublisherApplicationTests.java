ed8c03a45cca3e23b2f75614bf5e8dae954840e4
Marcos Henrique de Santana <76234459+marcoshssilva@users.noreply.github.com>
2025-08-16 20:18:05 -0300

    Create Tests to check if app starts with success

--- src/test/java/br/com/marcoshssilva/messagingpublisher/MessagingPublisherApplicationTests.java
+++ src/test/java/br/com/marcoshssilva/messagingpublisher/MessagingPublisherApplicationTests.java
@@ -1,13 +1,47 @@
 package br.com.marcoshssilva.messagingpublisher;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalManagementPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
-class MessagingPublisherApplicationTests {
+import static io.restassured.RestAssured.given;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class MessagingPublisherApplicationTests extends TestContainersConfiguration {
+
+	@LocalServerPort
+	private Integer serverPort;
+
+	@LocalManagementPort
+	private Integer managementServerPort;
+
+	@Value("${server.servlet.context-path}")
+	private String contextPath;
 
 	@Test
-	void contextLoads() {
+	void shouldOpenDefaultWebPage() {
+		RestAssured.baseURI = "http://localhost:" + serverPort + contextPath;
+		given()
+				.contentType(ContentType.JSON)
+				.when()
+				.get("/")
+				.then()
+				.statusCode(200);
+	}
+
+	@Test
+	void shouldReturnStatusStartupSuccesfully() {
+		RestAssured.baseURI = "http://localhost:" + managementServerPort;
+		given()
+				.contentType(ContentType.JSON)
+				.when()
+				.get("/actuator/health")
+				.then()
+				.statusCode(200);
 	}
 
 }
